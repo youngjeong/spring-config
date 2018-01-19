32037a96569d69eb4eb309745842ebf12e119454
youngjeong <ryj1549@gmail.com>
2018-01-19 17:01:09 +0900

    1

---
+++ src/main/java/io/pivotal/helloserver/HelloController.java
@@ -0,0 +1,25 @@
+package io.pivotal.helloserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${greeting}")
+    String greeting;
+
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam(value="name", defaultValue="World") String toWho) {
+        String result = String.format(greeting, toWho);
+        System.out.println(result);
+        return result;
+    }
+
+    @RequestMapping("/")
+    public String helloWord() {
+        return "Hello World!";
+    }
+}
